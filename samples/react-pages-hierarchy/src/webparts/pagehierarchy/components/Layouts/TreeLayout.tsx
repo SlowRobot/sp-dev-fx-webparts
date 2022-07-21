f3a8c176e682f88326145a0cb7211e552f9ecc11
SlowRobot <z5114667@student.unsw.edu.au>
2022-07-22 01:03:19 +1000

    changes to the tree view to remove extra border and background highlighting

--- samples/react-pages-hierarchy/src/webparts/pagehierarchy/components/Layouts/TreeLayout.tsx
+++ samples/react-pages-hierarchy/src/webparts/pagehierarchy/components/Layouts/TreeLayout.tsx
@@ -2,17 +2,33 @@ import * as React from 'react';
 import styles from './Layouts.module.scss';
 import { ILayoutProps } from './ILayoutProps';
 import * as strings from 'PageHierarchyWebPartStrings';
-import { Nav } from 'office-ui-fabric-react';
+import { Nav, INavStyles, INavStyleProps } from 'office-ui-fabric-react';
+
+const navStyles = (props: INavStyleProps): Partial<INavStyles> => ({
+  chevronIcon: [
+    props.isExpanded ? { transform: 'rotate(0deg)' } : { transform: 'rotate(-90deg)' }
+  ],
+  chevronButton: [
+    props.isSelected ? { backgroundColor: props.theme.semanticColors.buttonBackgroundPressed } : {},
+    {
+      selectors: {
+        '&::after': {
+          borderLeft: 'none',
+        }
+      },
+    }
+  ]
+});
 
 export const TreeLayout: React.FunctionComponent<ILayoutProps> = props => {
-    return(
+  return (
     <div className={styles.layouts}>
       {props.nav ? (
         <ul className={styles.listLayout}>
-          {<Nav groups={[{ links: [props.nav]}]} selectedKey={(props.pageId ?? '1').toString()} />}
+          {<Nav styles={navStyles} groups={[{ links: [props.nav] }]} selectedKey={(props.pageId ?? '1').toString()} />}
         </ul>
       ) : (
-          <span>{strings.Message_NoChildrenFound}</span>
-        )}
+        <span>{strings.Message_NoChildrenFound}</span>
+      )}
     </div>);
 };
